7feb302010431c985f91bde092c1d4a9d0da74b8
Joe <jecolasurdo@gmail.com>
2021-01-24 12:31:05 -0800

    Just adding an entrypoint as a scratch space.

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/jecolasurdo/projecteuler/p705"
+)
+
+func main() {
+	for i := 1111; i < 10000; i++ {
+		s := strconv.Itoa(i)
+		swaps := p705.InversionCount([]byte(s))
+		// fmt.Println(s, swaps)
+		fmt.Println(swaps)
+	}
 }
